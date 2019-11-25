921dfad6e8cd080450ddfc98d68955295e0edc97
nawal abdualghani <nawalabdualghani@gmail.com>
2019-11-25 07:32:44 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -11,9 +11,9 @@ function map(src, cb) {
 
 function reduce(src, cb, starting){
   let r = (!!starting) ? starting : src[0]
-  let i = (!!starting) ? 0 : 1
+  
 
-  for (; i < src.length; i++) {
+  for (let i = (!!starting) ? 0 : 1; i < src.length; i++) {
     r = cb(src[i], r)
   }
 
