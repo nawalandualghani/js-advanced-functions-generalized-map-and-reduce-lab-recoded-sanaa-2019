2ce73c6da3f49f970ed6f2b706ab2ffd8c4020e3
nawal abdualghani <nawalabdualghani@gmail.com>
2019-11-25 07:34:50 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-// Add your functions here
+function map(arr, cb) {
+  let r = []
+
+  for (let i = 0; i < arr.length; i++) {
+    let e = arr[i]
+    r.push(cb(e))
+  }
+
+  return r;
+}
+
+function reduce(arr, cb, starting){
+  let r = (!!starting) ? starting : arr[0]
+  for (let i = (!!starting) ? 0 : 1; i < arr.length; i++) {
+    r = cb(arr[i], r)
+  }
+
+  return r;
+}
